b806af945212dd15318fab190fce22e14033df89
Matt Burroughs <matt.burro@gmail.com>
2024-04-26 10:42:10 -0400

    Add delete product feature

---
+++ src/Services/Catalog/Catalog.API/Features/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Features.Products.DeleteProduct;
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}", async (Guid id, IMediator mediator) =>
+        {
+            var command = new DeleteProductCommand(id);
+
+            await mediator.Send(command);
+
+            return Results.Ok();
+        })
+            .WithName("DeleteProduct")
+            .WithSummary("Deletes a product")
+            .Produces(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
+
+public record DeleteProductResponse(bool Success);
