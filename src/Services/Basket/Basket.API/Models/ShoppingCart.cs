0e65f5d9bdb53b7424258cb424022ef6db923a78
Matt Burroughs <matt.burro@gmail.com>
2024-04-29 16:36:03 -0400

    Create domain models

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string Username { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+
+    public ShoppingCart(string username)
+    {
+        Username = username;
+    }
+
+    public ShoppingCart()
+    {
+    }
+}
