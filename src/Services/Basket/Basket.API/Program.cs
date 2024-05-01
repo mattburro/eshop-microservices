e5a216d8095ab1e3677977a2742dc845b4c3425c
Matt Burroughs <matt.burro@gmail.com>
2024-05-01 15:36:30 -0400

    Register and configure Carter and MediatR services

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,20 @@
+using Shared.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
