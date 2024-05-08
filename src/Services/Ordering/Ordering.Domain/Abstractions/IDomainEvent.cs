ea8e5d7a5972ad9834f00cecd9d6336e25702462
Matt Burroughs <matt.burro@gmail.com>
2024-05-08 17:15:43 -0400

    Create domain abstractions, models, and value objects

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+
+public class IDomainEvent : INotification
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime Timestamp => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Customer : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+
+    public static Customer Create(Guid id, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
+        ArgumentException.ThrowIfNullOrWhiteSpace(email, nameof(email));
+
+        return new Customer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+    }
+}
