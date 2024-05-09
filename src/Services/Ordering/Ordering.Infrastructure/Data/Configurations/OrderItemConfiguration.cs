12da3ca7aa140614353751c86c4e0d56e24b1a14
Matt Burroughs <matt.burro@gmail.com>
2024-05-09 15:38:34 -0400

    Create db context and entity configurations

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(oi => oi.Id);
+            builder.HasOne<Product>().WithMany().HasForeignKey(oi => oi.ProductId);
+            builder.Property(oi => oi.Quantity).IsRequired();
+            builder.Property(oi => oi.Price).IsRequired();
+        }
+    }
+}
