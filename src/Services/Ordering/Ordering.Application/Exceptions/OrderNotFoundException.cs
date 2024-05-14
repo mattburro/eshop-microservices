fb5162c68a7346672d1dc9e8300d18104b208f04
Matt Burroughs <matt.burro@gmail.com>
2024-05-14 14:37:11 -0400

    Finish update order feature

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿using Shared.Exceptions;
+
+namespace Ordering.Application.Exceptions;
+
+public class OrderNotFoundException(Guid id) : NotFoundException("Order", id)
+{
+}
--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -1,5 +1,7 @@
 ﻿global using Ordering.Application.Data;
 global using Ordering.Application.DTOs;
+global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+
 global using Shared.CQRS;
