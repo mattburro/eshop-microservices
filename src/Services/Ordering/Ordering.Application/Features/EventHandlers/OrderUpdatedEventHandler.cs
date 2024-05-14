7daf63e95872b6dcd168d1bcf54b773aebdc2f9e
Matt Burroughs <matt.burro@gmail.com>
2024-05-14 15:48:59 -0400

    Add order event handler stubs

---
+++ src/Services/Ordering/Ordering.Application/Features/EventHandlers/OrderUpdatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Features.EventHandlers;
+
+public class OrderUpdatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderUpdatedEvent>
+{
+    public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Domain Event handled: {notification.GetType().Name}");
+
+        return Task.CompletedTask;
+    }
+}
--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -3,5 +3,9 @@ global using Ordering.Application.DTOs;
 global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Events;
 
 global using Shared.CQRS;
+
+global using MediatR;
+global using Microsoft.Extensions.Logging;
