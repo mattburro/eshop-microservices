5be6bef024f0b48ce98f7ee9be4385bfd85dc66f
Matt Burroughs <matt.burro@gmail.com>
2024-05-14 16:34:00 -0400

    Add get orders by customer feature

---
+++ src/Services/Ordering/Ordering.Application/Features/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,18 @@
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Features.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerQueryHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId == query.CustomerId)
+            .OrderBy(o => o.OrderName)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByCustomerResult(orders.ToDto());
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,5 @@
+﻿namespace Ordering.Application.Features.Queries.GetOrdersByCustomer;
+
+public record GetOrdersByCustomerQuery(Guid CustomerId) : IQuery<GetOrdersByCustomerResult>;
+
+public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
