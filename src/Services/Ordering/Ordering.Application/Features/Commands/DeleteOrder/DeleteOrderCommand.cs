6d02349d4c9342dff52dabccd5eb2e419850f893
Matt Burroughs <matt.burro@gmail.com>
2024-05-14 14:47:45 -0400

    Add delete order feature

---
+++ src/Services/Ordering/Ordering.Application/Features/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Commands.DeleteOrder;
+
+public record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+
+public record DeleteOrderResult(bool IsSuccess);
+
+public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderCommandValidator()
+    {
+        RuleFor(c => c.OrderId).NotEmpty().WithMessage("OrderId is required");
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Ordering.Application.Features.Commands.DeleteOrder;
+
+public class DeleteOrderCommandHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var order = await dbContext.Orders.FindAsync(command.OrderId, cancellationToken);
+
+        if (order is null)
+        {
+            throw new OrderNotFoundException(command.OrderId);
+        }
+
+        dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderResult(true);
+    }
+}
