c8f4a7e6ac5b0a3e66a2bfed46053887531c1cba
Matt Burroughs <matt.burro@gmail.com>
2024-05-14 15:42:59 -0400

    Add order application validation and logging

--- src/Services/Ordering/Ordering.Application/ServiceExtensions.cs
+++ src/Services/Ordering/Ordering.Application/ServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Shared.Behaviors;
 using System.Reflection;
 
 namespace Ordering.Application;
@@ -7,7 +8,12 @@ public static class ServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
 
         return services;
     }
