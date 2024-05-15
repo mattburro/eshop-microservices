023c20955987b220bba65b13ca2323e6dd9c72ef
Matt Burroughs <matt.burro@gmail.com>
2024-05-15 16:12:52 -0400

    Add create order endpoint and stub out the rest

---
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -0,0 +1,27 @@
+﻿using Ordering.Application.Features.Commands.CreateOrder;
+
+namespace Ordering.API.Endpoints;
+
+public class CreateOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/orders", async (CreateOrderRequest request, IMediator mediator) =>
+        {
+            var command = request.Adapt<CreateOrderCommand>();
+
+            var result = await mediator.Send(command);
+
+            var response = result.Adapt<CreateOrderResponse>();
+
+            return Results.Created($"/orders/{response.Id}", response);
+        })
+            .WithName("CreateOrder")
+            .WithSummary("Creates an order")
+            .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
+
+public record CreateOrderRequest(OrderDto Order);
+public record CreateOrderResponse(Guid Id);
