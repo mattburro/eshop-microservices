c282d7d5d66d4f5d2d34117c487ca19f35f67c17
Matt Burroughs <matt.burro@gmail.com>
2024-05-15 16:26:45 -0400

    Create delete order endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,5 +1,28 @@
-﻿namespace Ordering.API.Endpoints;
+﻿
+using Ordering.Application.Features.Commands.DeleteOrder;
 
-public class DeleteOrder
+namespace Ordering.API.Endpoints;
+
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid id, IMediator mediator) =>
+        {
+            var command = new DeleteOrderCommand(id);
+
+            var result = await mediator.Send(command);
+
+            var response = result.Adapt<DeleteOrderResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("DeleteOrder")
+            .WithSummary("Deletes an order")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound);
+    }
 }
+
+public record DeleteOrderResponse(bool IsSuccess);
