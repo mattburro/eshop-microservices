fa2f150d14adc3a605ad37101a820c8b80e577df
Matt Burroughs <matt.burro@gmail.com>
2024-05-15 16:57:43 -0400

    Add get orders endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,5 +1,27 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Features.Queries.GetOrders;
+using Shared.Pagination;
 
-public class GetOrders
+namespace Ordering.API.Endpoints;
+
+public class GetOrders : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, IMediator mediator) =>
+        {
+            var query = new GetOrdersQuery(request);
+
+            var result = await mediator.Send(query);
+
+            var response = result.Adapt<GetOrdersResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("GetOrders")
+            .WithSummary("Gets all orders")
+            .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
 }
+
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
