320237ec7e5984989e5b0d46731bce2326423ed7
Matt Burroughs <matt.burro@gmail.com>
2024-05-15 16:42:36 -0400

    Add get orders by name endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -1,5 +1,27 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Features.Queries.GetOrderByName;
 
-public class GetOrdersByName
+namespace Ordering.API.Endpoints;
+
+public class GetOrdersByName : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/{orderName}", async (string orderName, IMediator mediator) =>
+        {
+            var query = new GetOrdersByNameQuery(orderName);
+
+            var result = await mediator.Send(query);
+
+            var response = result.Adapt<GetOrdersByNameResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("GetOrdersByName")
+            .WithSummary("Get all orders containing the given name")
+            .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound);
+    }
 }
+
+public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
