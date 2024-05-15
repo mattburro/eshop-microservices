c8f26103dc1e925f881f9ff2b300e3544a192f85
Matt Burroughs <matt.burro@gmail.com>
2024-05-15 16:20:45 -0400

    Create update order endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -1,5 +1,27 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Features.Commands.CreateOrder;
 
-public class UpdateOrder
+namespace Ordering.API.Endpoints;
+
+public class UpdateOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders/{id}", async (UpdateOrderRequest request, IMediator mediator) =>
+        {
+            var command = request.Adapt<CreateOrderCommand>();
+
+            var result = await mediator.Send(command);
+
+            var response = result.Adapt<UpdateOrderResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("UpdateOrder")
+            .WithSummary("Updates an order")
+            .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
 }
+
+public record UpdateOrderRequest(OrderDto Order);
+public record UpdateOrderResponse(bool IsSuccess);
