1d8d7f4ae03a4711a8a87c44b532b7c74f14fb0c
Matt Burroughs <matt.burro@gmail.com>
2024-05-17 15:16:31 -0400

    Add shared messaging project, and integration events, configure mass transit, and update event consumer/publisher appsettings

---
+++ src/Shared/Shared.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Messaging.Events;
+
+public abstract record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime Timestamp => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
---
+++ src/Shared/Shared.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Shared.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null)
+            {
+                config.AddConsumers(assembly);
+            }
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), hostConfigurator =>
+                {
+                    hostConfigurator.Username(configuration["MessageBroker:Username"]!);
+                    hostConfigurator.Password(configuration["MessageBroker:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
