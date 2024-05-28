7b7d05e9be69c39cae95d2e00d73ac61c863d108
Matt Burroughs <matt.burro@gmail.com>
2024-05-28 10:23:08 -0400

    Add yarp and map catalog microservice

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 // Configure HTTP request pipeline
+app.MapReverseProxy();
 
 app.Run();
